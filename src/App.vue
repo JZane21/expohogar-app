95d66705fe670c01815314083dd6635faa41bdb5
Jose Carrasco <jscarrasco18@outlook.com>
2023-11-14 20:50:19 -0400

    creando un proyecto en VueJS

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="fixed top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center">
+    <div class="flex flex-row justify-center items-center">
+      <a class="w-100px" href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="h-[6em] p-[1.5em] will-change-auto transition-all hover:fill-blue-400" alt="Vite logo" />
+      </a>
+      <a class="w-max" href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="h-[6em] p-[1.5em] will-change-auto transition-all hover:fill-green-400" alt="Vue logo" />
+      </a>
+    </div>
+    <HelloWorld msg="Vite + Vue" />
+  </div>
+</template>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">import { Ref, ref } from '@vue/runtime-dom';
+  const num1:Ref<number> = ref<number>(0);
+  const num2:Ref<number> = ref<number>(0);
+  const result:Ref<number> = ref<number>(0);
+  console.log(num1.value);
+  const sumar = () => {
+    result.value = num1.value + num2.value;
+    console.log(result.value);
+  }
+
+  const numberCondition: RegExp = /^-?\d+$/;
+
+  const changeValueNum1 = (event:any) => {
+    const value:string = event.target.value;
+    if(numberCondition.test(value)){
+      num1.value = Number(value);
+      console.log(num1.value);
+    }else{
+      console.log("no es un numero");
+    }
+  };
+
+  const changeValueNum2 = (event:any) => {
+    const value:string = event.target.value;
+    if(numberCondition.test(value)){
+      num2.value = Number(value);
+      console.log(num2.value);
+    }else{
+      console.log("no es un numero");
+    }
+  };
+</script>
+<template>
+  <div class="container mx-auto mt-12">
+    <h1 class="text-3xl font-semibold mb-6">Calculadora Vue 3</h1>
+    <div class="mb-4">
+      <label for="num1" class="block text-sm font-medium text-gray-700">Número 1:</label>
+      <input @input="changeValueNum1" @value="num1"
+      type="text" class="mt-1 p-2 border border-gray-300 rounded-md w-full" />
+    </div>
+    <div class="mb-4">
+      <label for="num2" class="block text-sm font-medium text-gray-700">Número 2:</label>
+      <input @input="changeValueNum2" @value="num2"
+      type="text" class="mt-1 p-2 border border-gray-300 rounded-md w-full" />
+    </div>
+    <button @click="sumar" class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
+      Sumar
+    </button>
+    <div class="mt-6">
+      <h2 class="text-xl font-semibold mb-2">Resultado:</h2>
+      <p class="text-lg font-medium">{{ result }}</p>
+    </div>
+  </div>
+</template>
