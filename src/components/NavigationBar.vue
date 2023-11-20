b44bffc6f6b4a37149065e1b56f8a4ef45896794
Amy312 <katsukiamy1.gmail.com>
2023-11-20 01:18:29 -0400

    fixed with routes and parts

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,11 +1,13 @@
 <template>
-  <nav class="w-full bg-red-800 py-4 text-white flex justify-end items-center">
+  <nav class="w-full bg-red-800 py-4 text-white flex justify-between items-center px-10">
     <div class="flex items-center gap-2">
-      <button class="px-3 py-2 rounded-md hover:bg-red-700">Inicio</button>
-      <button class="px-3 py-2 rounded-md hover:bg-red-700">Servicios</button>
-      <button class="px-3 py-2 rounded-md hover:bg-red-700">Productos</button>
-      <button class="px-3 py-2 rounded-md hover:bg-red-700">Acerca de</button>
-      <button class="px-3 py-2 rounded-md hover:bg-red-700">Contacto</button>
+      <router-link to="/fase-inicio" class="px-3 py-2 rounded-md hover:bg-red-700">Fase de Inicio</router-link>
+      <router-link to="/fase-planificacion" class="px-3 py-2 rounded-md hover:bg-red-700">Fase de Planificación</router-link>
+      <router-link to="/fase-desarrollo" class="px-3 py-2 rounded-md hover:bg-red-700">Fase de Diseño y desarrollo</router-link>
+      <router-link to="/fase-control" class="px-3 py-2 rounded-md hover:bg-red-700">Fase de Control de Calidad e implementación</router-link>
+      <router-link to="/fase-cierre" class="px-3 py-2 rounded-md hover:bg-red-700">Fase de Cierre</router-link>
+      <router-link to="/sobre-nosotros" class="px-3 py-2 rounded-md hover:bg-red-700">Sobre Nosotros</router-link>
+
     </div>
     <button class="bg-blue-500 w-12 h-12 rounded-full flex justify-center items-center hover:bg-blue-600 ml-4">
       <span class="text-lg">U</span> <!-- Icono o iniciales del usuario aquí -->
