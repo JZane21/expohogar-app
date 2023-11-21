e54b1237106eea0e3c0b3b9b0236ac6070489c05
NatyBilbao <natabilbaocano18@gmail.com>
2023-11-21 02:56:48 -0400

    TDR y Fase de Inicio

--- src/pages/FaseInicio.vue
+++ src/pages/FaseInicio.vue
@@ -1,47 +1,119 @@
 <template>
-  <div class="bg-slate-100 w-full min-h-screen p-10">
-    <h1 class="text-3xl font-bold">Fase de Inicio</h1>
+  <div class="overflow-y-auto h-screen p-4">
+      <!-- Sección de Terminos de Referencia -->
+      <h2 class="text-xl font-semibold mb-4">Terminos de Referencia</h2>
+      <div class="overflow-y-auto max-h-96 p-4 border border-gray-300 rounded-lg mb-6">
+          <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <!-- Antecedentes y Justificación -->
+          <div class="border border-gray-300 p-4 rounded-lg">
+              <h2 class="text-lg font-semibold mb-2">1. Antecedentes y Justificación</h2>
+              <p>- EXPOHOGAR es un evento consolidado de productos para el hogar.</p>
+              <p><br>- Éxito en eventos físicos, pero necesidad de presencia digital.</p>
+              <p><br>- Mejora de visibilidad y accesibilidad en la era digital.</p>
+              <p><br>- Alcance global a través de una plataforma web.</p>
+          </div>
+      
+          <!-- Objetivos -->
+          <div class="border border-gray-300 p-4 rounded-lg">
+              <h2 class="text-lg font-semibold mb-2">2. Objetivos</h2>
+              <p>- Consultoría para diseñar y desarrollar un sitio web para EXPOHOGAR.</p>
+              <p><br>- Información del evento, registro de participantes, promoción de expositores.</p>
+          </div>
+      
+          <!-- Alcance de los Servicios -->
+          <div class="border border-gray-300 p-4 rounded-lg">
+              <h2 class="text-lg font-semibold mb-2">3. Alcance de los Servicios</h2>
+              <p>- Desarrollo del sitio web, arquitectura, usabilidad, transacciones.</p>
+              <p><br>- Optimización para SEO y adaptabilidad móvil.</p>
+              <p><br>- Mantenimiento, soporte, hosting y seguridad.</p>
+              <p><br>- Implementación de capacidades transaccionales y bases de datos.</p>
+          </div>
+      
+          <!-- Actividades Clave -->
+          <div class="border border-gray-300 p-4 rounded-lg">
+              <h2 class="text-lg font-semibold mb-2">4. Actividades Clave</h2>
+              <p>- Planificación y desarrollo del sitio web.</p>
+              <p><br>- Coordinación con el equipo de EXPOHOGAR.</p>
+              <p><br>- Implementación, prueba y capacitación.</p>
+              <p><br>- Puntos de revisión clave.</p>
+          </div>
+      
+          <!-- Resultados y Productos Esperados -->
+          <div class="border border-gray-300 p-4 rounded-lg">
+              <h2 class="text-lg font-semibold mb-2">5. Resultados y Productos Esperados</h2>
+              <p>- Sitio web funcional, fácil de usar y cumplimiento de requisitos.</p>
+              <p><br>- Documentación técnica y manuales de usuario.</p>
+              <p><br>- Informes de rendimiento y uso del sitio.</p>
+          </div>
+      
+          <!-- Otros Requisitos -->
+          <div class="border border-gray-300 p-4 rounded-lg">
+              <h2 class="text-lg font-semibold mb-2">6. Otros Requisitos</h2>
+              <p>- Colaboración constante y reuniones periódicas.</p>
+              <p><br>- Alineación con la identidad de EXPOHOGAR.</p>
+              <p><br>- Adaptabilidad y escalabilidad del sitio.</p>
+              <p><br>- Cumplimiento de normativas y accesibilidad.</p>
+              <p><br>- Formación y transferencia de conocimientos.</p>
+              <p><br>- Soporte post-lanzamiento y revisión periódica.</p>
+          </div>
+      
+          <!-- Supervisión e Informes -->
+          <div class="border border-gray-300 p-4 rounded-lg">
+              <h2 class="text-lg font-semibold mb-2">7. Supervisión e Informes</h2>
+              <p>- Informes a Marcelo Ticona y coordinador de EXPOHOGAR.</p>
+          </div>
 
-    <h1 class="text-xl font-bold mt-4">{{ projectName }}</h1>
-    <p>{{ projectDescription }}</p>
-
-    <section>
-      <h2 class="text-xl font-bold mt-4">Propósito</h2>
-      <p>{{ purpose }}</p>
-    </section>
-
-    <section>
-      <h2 class="text-xl font-bold mt-4">Viabilidad del Proyecto</h2>
-      <p>{{ projectViability }}</p>
-    </section>
-
-    <section>
-      <h2 class="text-xl font-bold mt-4">Objetivos, Alcance y Requisitos</h2>
-      <p>{{ projectObjectives }}</p>
-    </section>
-
-    <section>
-      <h2 class="text-xl font-bold mt-4">Requerimientos del Proyecto</h2>
-      <p>{{ projectRequirements }}</p>
-    </section>
+          <!-- Calendario de Pagos -->
+          <div class="border border-gray-300 p-4 rounded-lg">
+              <h2 class="text-lg font-semibold mb-2">8. Calendario de Pagos</h2>
+              <p>- Condiciones basadas en hitos o entregables del proyecto.</p>
+              <p><br>- No se esperan pagos por adelantado, propuesta de costos competitiva.</p>
+          </div>
+      </div>
   </div>
+
+      <!-- Fase de Inicio -->
+      <h2 class="text-xl font-semibold mb-4">Fase de Inicio</h2>
+      <div class="overflow-y-auto max-h-96 p-4 border border-gray-300 rounded-lg mb-6">
+          <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+              
+              <!-- Desarrollo del Sitio Web para EXPOHOGAR -->
+              <div class="border border-gray-300 p-4 rounded-lg">
+                  <h3 class="text-lg font-semibold mb-2">Desarrollo del Sitio Web para EXPOHOGAR</h3>
+                  <p>El proyecto de desarrollo del sitio web para EXPOHOGAR inicia con la definición de su propósito, objetivos y partes interesadas clave.</p>
+              </div>
+
+              <!-- Propósito -->
+              <div class="border border-gray-300 p-4 rounded-lg">
+                  <h3 class="text-lg font-semibold mb-2">Propósito</h3>
+                  <p>El propósito es proporcionar información clara sobre el evento, facilitar el registro de participantes y destacar a los expositores. Los grupos de partes interesadas incluyen asistentes, expositores, organizadores, patrocinadores, inversores y equipos internos.</p>
+              </div>
+
+              <!-- Viabilidad del Proyecto -->
+              <div class="border border-gray-300 p-4 rounded-lg">
+                  <h3 class="text-lg font-semibold mb-2">Viabilidad del Proyecto</h3>
+                  <p>La viabilidad del proyecto se evalúa considerando recursos humanos y tecnológicos. Se cuenta con equipos especializados, roles definidos y se adopta la metodología ágil SCRUM. Se detallan los roles y responsabilidades del personal, así como la elección de tecnologías como Vue.js para el frontend y una infraestructura escalable.</p>
+              </div>
+
+              <!-- Objetivos, Alcance y Requisitos -->
+              <div class="border border-gray-300 p-4 rounded-lg">
+                  <h3 class="text-lg font-semibold mb-2">Objetivos, Alcance y Requisitos</h3>
+                  <p>Se establecen objetivos, alcance y requisitos iniciales en un documento de inicio de proyecto. Los objetivos incluyen diseñar una página intuitiva, ofrecer servicios de registro y promoción de expositores. El alcance inicial se centra en La Paz, con un manual de uso y menú de opciones. Se establecen límites, como el uso de datos de prueba y un período de soporte de tres meses.</p>
+              </div>
+
+              <!-- Requerimientos del Proyecto -->
+              <div class="border border-gray-300 p-4 rounded-lg">
+                  <h3 class="text-lg font-semibold mb-2">Requerimientos del Proyecto</h3>
+                  <p>Los requerimientos del proyecto incluyen recursos visuales, información detallada para el registro, reuniones semanales de avance y la exclusión de funciones de compra en la página. </p>
+              </div>
+          </div>
+      </div>
+  </div>
+
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      projectName: "Desarrollo del Sitio Web para EXPOHOGAR",
-      projectDescription: "El proyecto de desarrollo del sitio web para EXPOHOGAR inicia con la definición de su propósito, objetivos y partes interesadas clave.",
-      purpose: "El propósito es proporcionar información clara sobre el evento, facilitar el registro de participantes y destacar a los expositores. Los grupos de partes interesadas incluyen asistentes, expositores, organizadores, patrocinadores, inversores y equipos internos.",
-      projectViability: "La viabilidad del proyecto se evalúa considerando recursos humanos y tecnológicos. Se cuenta con equipos especializados, roles definidos y se adopta la metodología ágil SCRUM. Se detallan los roles y responsabilidades del personal, así como la elección de tecnologías como Vue.js para el frontend y una infraestructura escalable.",
-      projectObjectives: "Se establecen objetivos, alcance y requisitos iniciales en un documento de inicio de proyecto. Los objetivos incluyen diseñar una página intuitiva, ofrecer servicios de registro y promoción de expositores. El alcance inicial se centra en La Paz, con un manual de uso y menú de opciones. Se establecen límites, como el uso de datos de prueba y un período de soporte de tres meses.",
-      projectRequirements: "Los requerimientos del proyecto incluyen recursos visuales, información detallada para el registro, reuniones semanales de avance y la exclusión de funciones de compra en la página. Se proporcionará enlaces a sitios oficiales para compras y contacto con patrocinadores o clientes.",
-    };
-  },
-};
+<script setup lang="ts">
 </script>
 
-<style scoped>
-/* Estilos específicos del componente, si es necesario */
+<style>
 </style>
