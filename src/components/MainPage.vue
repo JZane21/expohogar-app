917d62304a93861629d4c65f26bd901aa80d7ec4
Amy312 <katsukiamy1.gmail.com>
2023-11-21 08:18:26 -0400

    finished

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,20 +1,13 @@
 <script setup lang="ts">
-import NavigationBar from './NavigationBar.vue';
-
+import NavigationBar from "./NavigationBar.vue";
 </script>
 
 <template>
   <div class="flex flex-col min-h-screen w-full justify-start">
     <header>
       <NavigationBar />
-      <RouterView/>
+      <RouterView />
     </header>
-
-    <footer class="mt-auto p-4 bg-gray-200 w-full">
-      <p class="text-center text-sm">
-        © 2023 ExpoHogar App. Todos los derechos reservados.
-      </p>
-    </footer>
   </div>
 </template>
 
@@ -24,4 +17,11 @@ footer {
   bottom: 0;
   width: 100%;
 }
+/*
+<footer class="mt-auto p-4 bg-gray-200 w-full">
+  <p class="text-center text-sm">
+    © 2023 ExpoHogar App. Todos los derechos reservados.
+  </p>
+</footer>
+*/
 </style>
